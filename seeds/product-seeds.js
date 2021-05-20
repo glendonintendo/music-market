f01b58d48b041d0c6e7d1ceaeffd71450932efa3
glendo <gleneggers64@gmail.com>
2021-05-20 13:13:27 -0500

    add product data for seed

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,16 +2,60 @@ const { Product } = require('../models');
 
 const productData = [
     {
-
+        product_name: "Stevie's Guitar",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 4000,
+        stock: 1,
+        category_id: 5
     },
     {
-
+        product_name: "Ray's Piano",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 10000,
+        stock: 1,
+        category_id: 5
     },
     {
-
+        product_name: "Guitar Picks",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 10,
+        stock: 50,
+        category_id: 2
     },
     {
-
+        product_name: "Band Tee",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 20,
+        stock: 15,
+        category_id: 3
+    },
+    {
+        product_name: "New LP",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 15,
+        stock: 100,
+        category_id: 4
+    },
+    {
+        product_name: "Best Hits",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 20,
+        stock: 10,
+        category_id: 4
+    },
+    {
+        product_name: "Handcrafted Bow",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 100,
+        stock: 3,
+        category_id: 2
+    },
+    {
+        product_name: "Saxaphone",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        price: 150,
+        stock: 8,
+        category_id: 1 
     }
 ];
 
