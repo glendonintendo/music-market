b39013d90561e8704a07d30de928d056dc27ec2e
glendo <gleneggers64@gmail.com>
2021-05-21 14:23:55 -0500

    edit formatting

--- models/Cart.js
+++ models/Cart.js
@@ -1,32 +1,32 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
-class Cart extends Model { }
+class Cart extends Model {}
 
-Cart.init({
-    //defining columns
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true,
-    },
-    user_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'user',
-            key: 'id'
+Cart.init(
+    {
+        //defining columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        },
+        product_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'product',
+                key: 'id'
+            }
         }
     },
-    product_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'product',
-            key: 'id'
-        }
-    }
-},
     {
         sequelize,
         timestamps: false,
