1b24266059974e728525771663cd8a5365cb0101
glendo <gleneggers64@gmail.com>
2021-05-21 14:24:26 -0500

    edit formatting;; add description column

--- models/Product.js
+++ models/Product.js
@@ -1,44 +1,48 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection');
 
-class Product extends Model { }
+class Product extends Model {}
 
-Product.init({
-    // defining columns
-    id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    product_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    price: {
-        type: DataTypes.DECIMAL,
-        allowNull: false,
-        validate: {
-            isDecimal: true
+Product.init(
+    {
+        // defining columns
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        product_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        price: {
+            type: DataTypes.DECIMAL,
+            allowNull: false,
+            validate: {
+                isDecimal: true
+            }
+        },
+        stock: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            defaultValue: [10],
+            validate: {
+                isNumeric: true
+            }
+        },
+        category_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'category',
+                key: 'id'
+            }
         }
     },
-    stock: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        defaultValue: [10],
-        validate: {
-            isNumeric: true
-        }
-    },
-    category_id: {
-        type: DataTypes.INTEGER,
-        references: {
-            model: 'category',
-            key: 'id'
-        }
-    }
-},
     {
         sequelize,
         timestamps: false,
