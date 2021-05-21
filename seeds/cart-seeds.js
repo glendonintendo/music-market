ad60d8e23d919af69cec701d854b7b595b50804f
glendo <gleneggers64@gmail.com>
2021-05-21 14:10:13 -0500

    add cart seed data

--- seeds/cart-seeds.js
+++ seeds/cart-seeds.js
@@ -2,7 +2,20 @@ const { Cart } = require('../models');
 
 const cartData = [
     {
-
+        user_id: 1,
+        product_id: 1
+    },
+    {
+        user_id: 1,
+        product_id: 3
+    },
+    {
+        user_id: 2,
+        product_id: 4
+    },
+    {
+        user_id: 2,
+        product_id: 5 
     }
 ];
 
