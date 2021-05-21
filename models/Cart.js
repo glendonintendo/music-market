bbc9c9cbc86657389a9cd97749effd70dae9a151
glendo <gleneggers64@gmail.com>
2021-05-20 20:42:52 -0500

    change sequelize require statement

--- models/Cart.js
+++ models/Cart.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connections');
+const sequelize = require('../config/connection');
 
 class Cart extends Model { }
 
