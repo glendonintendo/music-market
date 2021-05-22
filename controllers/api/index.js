cf156d0c838b9ec93901ad9947a2cac796968331
glendo <gleneggers64@gmail.com>
2021-05-21 19:52:35 -0500

    add model api routes to router

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
 
+const userRoutes = require('./user-routes');
+const categoryRoutes = require('./category-routes');
+const productRoutes = require('./product-routes');
+const tagRoutes = require('./tag-routes');
+
+router.use('/users', userRoutes);
+router.use('/categories', categoryRoutes);
+router.use('./products', productRoutes);
+router.use('./tags', tagRoutes);
+
 module.exports = router;
