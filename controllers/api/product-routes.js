2a89a4af5d6297e1f078237246944c58db327d0d
glendo <gleneggers64@gmail.com>
2021-05-21 20:18:58 -0500

    add get product by id; add include category and tags in get routes

--- controllers/api/product-routes.js
+++ controllers/api/product-routes.js
@@ -1,12 +1,55 @@
 const router = require('express').Router();
-const { Product } = require('../../models');
+const { Product, Category, Tag, ProductTag } = require('../../models');
 
 router.get('/', (req, res) => {
-    Product.findAll()
+    Product.findAll({
+        attributes: [
+                'id',
+                'product_name',
+                'price',
+                'stock'
+        ],
+        include: [
+            {
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name'],
+                through: ProductTag
+            }
+        ]
+    })
         .then(dbProductData => res.json(dbProductData))
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
+        });
+});
+
+
+router.get('/:id', (req, res) => {
+    Product.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Category,
+                attributes: ['id', 'category_name']
+            },
+            {
+                model: Tag,
+                attributes: ['tag_name'],
+                through: ProductTag
+            }
+        ]
+    })
+        .then(dbProductData => res.json(dbProductData))
+        .catch(err => {
+            console.log(err);
+            res.json(err);
         });
 });
 
