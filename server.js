7cc9d0f47f14a69e33cff7e3d0527860ac73fca9
glendo <gleneggers64@gmail.com>
2021-05-21 19:36:17 -0500

    add session and passport as middleware

--- server.js
+++ server.js
@@ -1,12 +1,37 @@
+const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connections');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const initializePassport = require('./config/passport-config');
+initializePassport(
+  passport, 
+  email => users.find(user => user.email === email),
+  id => users.find(user => user.id === id)
+);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: false,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(routes);
 
