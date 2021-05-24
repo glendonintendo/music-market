dc65e9e79f47492f1d18765eadfca99c0902574c
peter <ptmartinezsound@gmail.com>
2021-05-23 20:51:49 -0500

    added home routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+
+router.get('/', (req, res) => {
+    res.render('homepage');
+});
+
+router.get('/login', (req, res) => {
+    res.render('login')
+});
+
+
+module.exports = router;
