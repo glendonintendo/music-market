3df75034437f902cb3084c754d7c1a57ed514efd
peter <ptmartinezsound@gmail.com>
2021-05-25 11:41:09 -0500

    home routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,70 @@
 const router = require('express').Router();
+const sequelize = require('../config/connections');
+const { Category } = require('../models');
 
+//get all categories 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    console.log('=================');
+    Category.findAll({
+        attributes: [
+            'id',
+            'category_name'
+        ]
+    })
+        .then(dbCategoryData => {
+            const categories = dbCategoryData.map(post => post.get({ plain: true }));
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
-router.get('/login', (req, res) => {
-    res.render('login')
+//browse
+router.get('categories/:id', (req, res) => {
+    Category.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'category_name'
+        ]
+    })
+        .then(dbCategoryData => {
+            if (!dbCategoryData) {
+                res.status(404).json({ message: 'No Category found with this id' });
+                return;
+            }
+
+            const categories = dbCategoryData.get({ plain: true });
+
+            res.render('browse', {
+                categories,
+                loggedIn: req.session.loggedIn
+            });
+        });
 });
+
+//login
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+
 
 
 module.exports = router;
+
+
+
+
