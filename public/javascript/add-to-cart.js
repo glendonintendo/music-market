97afb8375cb2d85a335b64279ce42d0959490308
BrandonShoemaker <brandon.shoemaker2080@gmail.com>
2021-05-27 18:03:59 -0500

    finished adding products to cart

--- public/javascript/add-to-cart.js
+++ public/javascript/add-to-cart.js
@@ -2,32 +2,30 @@
 // let productPrices = document.getElementsByClassName("cartProductPrice");
 
 function addItemToCart(e){
+    let divEl = document.createElement('div');
+    let cartProductQuantity = document.createElement('div');
+    let cartProductName = document.createElement('div');
+    let removeItem = document.createElement('div');
+
     console.log(this);
-    console.log(e.target);
-    if(e.target.className == 'card'){
-        alert('12345');
-        let divEl = document.createElement('div');
-        let cartProductQuantity = document.createElement('div');
-        let cartProductName = document.createElement('div');
-        let removeItem = document.createElement('div');
+    cartProductName.innerHTML = this.childNodes[1].childNodes[0].data;
+    cartProductQuantity.innerHTML = /*document.querySelector('.card-section').innerHTML*/ 'x5';
+    removeItem.innerHTML = '&times;';
 
-        let cardEls = e.currentTarget.childNodes;
-        console.log(cardEls);
-        console.log(this);
-        cartProductName.innerHTML = cardEls[1].childNodes[0].data;
-        cartProductQuantity.innerHTML = /*document.querySelector('.card-section').innerHTML*/ 'x5';
-        removeItem.innerHTML = '&times;';
+    cartProductQuantity.className = 'cartProductQuantity';
+    cartProductName.className = 'cartProductName';
+    removeItem.className = 'removeItem';
+    
+    divEl.appendChild(cartProductQuantity);
+    divEl.appendChild(cartProductName);
+    divEl.appendChild(removeItem);
 
-        cartProductQuantity.className = 'cartProductQuantity';
-        cartProductName.className = 'cartProductName';
-        removeItem.className = 'removeItem';
-        
-        divEl.appendChild(cartProductQuantity);
-        divEl.appendChild(cartProductName);
-        divEl.appendChild(removeItem);
+    document.querySelector('#cart').appendChild(divEl);
 
-        document.querySelector('#cart').appendChild(divEl);
-    }
 }
 
-document.querySelector(".card").addEventListener("click", addItemToCart);
+document.querySelectorAll(".card").forEach(element => {
+    element.addEventListener("click", addItemToCart); 
+});
+
+console.log('js is loaded');
