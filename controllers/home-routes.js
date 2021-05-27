d2721094eb562ec310c0104cae28865da5324874
glendo <gleneggers64@gmail.com>
2021-05-27 13:11:32 -0500

    add tags and categories to browse route

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Category } = require('../models');
+const { Category, Product, Tag, ProductTag } = require('../models');
 
-//get all categories 
+// homepage
 router.get('/', (req, res) => {
     console.log('*************************');
     Category.findAll({
@@ -26,42 +26,66 @@ router.get('/', (req, res) => {
         });
 });
 
-//browse
-router.get('categories/:id', (req, res) => {
-    Category.findOne({
-        where: {
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'category_name'
-        ]
-    })
-        .then(dbCategoryData => {
-            if (!dbCategoryData) {
-                res.status(404).json({ message: 'No Category found with this id' });
-                return;
-            }
+// browse page (by category)
+router.get('/browse/:id', (req, res) => {
+    const categoryPromise = 
+        Category.findAll({
+            attributes: [
+                'id',
+                'category_name'
+            ]
+        });
 
-            const categories = dbCategoryData.get({ plain: true });
+    const tagPromise = 
+        Tag.findAll();
+
+    const productPromise = 
+        Product.findAll({
+            where: {
+                category_id: req.params.id
+            },
+            attributes: [
+                    'id',
+                    'product_name',
+                    'description',
+                    'price',
+                    'stock',
+                    'image_path'
+            ],
+            include: [
+                {
+                    model: Category,
+                    attributes: ['id', 'category_name']
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name'],
+                    through: ProductTag
+                }
+            ]
+        });
+        
+    Promise.all([categoryPromise, productPromise, tagPromise])
+        .then(data => {
+            const categories = data[0].map(categories => categories.get({ plain: true }));
+            const products = data[1].map(products => products.get({ plain: true }));
+            const tags = data[2].map(tags => tags.get({ plain: true }));
+
+            console.log(categories);
+            console.log(products);
+            console.log(tags);
 
             res.render('browse', {
                 layout: false,
                 categories,
+                products,
+                tags,
                 loggedIn: req.session.loggedIn
             });
         });
 });
 
-//login
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-    res.render('login', { layout: false });
-});
-
+// contact page
 router.get('/contact', (req, res) => {
     console.log('*************************');
     Category.findAll({
