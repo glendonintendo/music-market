d6701615e7c5abed71d4d6a079c7cf9bb90b806a
glendo <gleneggers64@gmail.com>
2021-05-30 15:03:12 -0500

    button directs to browse all

--- public/javascript/homepage-start.js
+++ public/javascript/homepage-start.js
@@ -1,7 +1,7 @@
 async function startButtonHandler(event) {
     event.preventDefault();
 
-    // redirect to browse or optionally redirect to login? - could be a conditional depeding on if they were loggedIn
+    document.location.replace('/browse');
 };
 
 document.querySelector('#start-btn').addEventListener('click', startButtonHandler);
