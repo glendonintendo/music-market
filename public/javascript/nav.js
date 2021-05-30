f5b5f25756ef7d17bfdf9a270db944be03438948
glendo <gleneggers64@gmail.com>
2021-05-30 15:04:42 -0500

    add redirect to browse onclick browse in nav

--- public/javascript/nav.js
+++ public/javascript/nav.js
@@ -10,5 +10,12 @@ function contactNavHandler(event) {
     document.location.replace('/contact');
 };
 
+function browseNavHandler(event) {
+    event.preventDefault();
+
+    document.location.replace('/browse');
+};
+
 document.querySelector('#homepage-nav').addEventListener('click', homepageNavHandler);
 document.querySelector('#contact-nav').addEventListener('click', contactNavHandler);
+document.querySelector('#browse-nav').addEventListener('click', browseNavHandler);
